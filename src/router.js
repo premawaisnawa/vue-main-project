44e31b687732365355f430a2039519668d2ccd76
Kade Prema Waisnawa <premawaisnawa@gmail.com>
2020-12-12 19:40:20 +0800

    wip fix error router component

--- src/router.js
+++ src/router.js
@@ -4,21 +4,21 @@ import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
-import NotFound from './pages/requests/NotFound.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
 	history: createWebHistory(),
 	routes: [
-        { path: '/', redirect:'/coaches' },
+		{ path: '/', redirect: '/coaches' },
 		{ path: '/coaches', component: CoachesList },
 		{
 			path: '/coaches/:id',
 			component: CoachDetail,
 			children: [ { path: 'contact', component: ContactCoach } ]
-        },
-        { path: '/register', component: CoachRegistration },
-        { path: '/request', component: RequestsReceived },
-        { path: '/:notFound(.*)', component: NotFound },
-        
+		},
+		{ path: '/register', component: CoachRegistration },
+		{ path: '/request', component: RequestsReceived },
+		{ path: '/:notFound(.*)', component: NotFound }
 	]
 });
+export default router;
